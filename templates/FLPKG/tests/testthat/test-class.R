4ab3b2c86774f00cf45a35978cdc2da1d627e1e6
Iago Mosqueira <iago@mosqueira.eu>
2016-09-07 15:37:25 +0200

    Adding tests template

---
+++ templates/FLPKG/tests/testthat/test-class.R
@@ -0,0 +1,36 @@
+# test-class.R - DESC
+# FLPKG/tests/testthat/test-class.R
+
+# Copyright %USER%, %YEAR%
+# Author: %USER% <%EMAIL%>
+#
+# Distributed under the terms of the %LICENSE%
+
+# CONTEXT class
+context("CLASS")
+
+# TEST prototype
+out <- new("CLASS")
+
+test_that(class(out), equals())
+test_that(validObject(out), is_true())
+
+# CONTEXT class constructor
+context("CLASS constructor")
+
+# TEST constructor
+test_constructor <- function(msg, method, object) {
+
+  out <- do.call(method, list(object=object))
+
+  # check validObject
+  test_that(validObject(out), is_true())
+
+  # check class
+  test_that(class(out), equals())
+}
+
+# TEST
+test_constructor()
+
+# class accessors
--- templates/FLPKG/tests/testthat/testthat.R
+++
@@ -1,3 +0,0 @@
-library(testthat)
-library(FLasher)
-test_check("FLasher")
