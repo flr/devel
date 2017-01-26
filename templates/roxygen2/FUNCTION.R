10c4b8a574e11893f6b706d57710cfbf6d0e6ded
Iago Mosqueira <iago@mosqueira.eu>
2017-01-26 16:36:52 +0100

    Various cahnges

---
+++ templates/roxygen2/FUNCTION.R
@@ -0,0 +1,35 @@
+# FUNCTION.R - DESC
+# /R/FUNCTION.R
+
+# Copyright 2003-2014 FLR Team. Distributed under the GPL 2 or later
+# Maintainer: FLR Team <flr-team@flr-project.org>
+# Soundtrack:
+# Notes:
+
+#' A function to
+#'
+#' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend
+#' odio ac rutrum luctus. Aenean placerat porttitor commodo. Pellentesque eget porta
+#' libero. Pellentesque molestie mi sed orci feugiat, non mollis enim tristique. 
+#' Suspendisse eu sapien vitae arcu lobortis ultrices vitae ac velit. Curabitur id 
+#' nunc euismod ante fringilla lobortis. Aliquam ullamcorper in diam non placerat. 
+#'
+#' Aliquam sagittis feugiat felis eget consequat. Praesent eleifend dolor massa, 
+#' vitae faucibus justo lacinia a. Cras sed erat et magna pharetra bibendum quis in 
+#' mi. Sed sodales mollis arcu, sit amet venenatis lorem fringilla vel. Vivamus vitae 
+#' ipsum sem. Donec malesuada purus at libero bibendum accumsan. Donec ipsum sapien, 
+#' feugiat blandit arcu in, dapibus dictum felis. 
+#'
+#' @param PARAM Lorem ipsum dolor sit amet
+#'
+#' @return RETURN Lorem ipsum dolor sit amet
+#'
+#' @name FUNCTION
+#' @rdname FUNCTION
+#' @aliases FUNCTION
+#'
+#' @author The FLR Team
+#' @seealso \link{FLComp}
+#' @keywords classes
+#' @examples
+#' 
